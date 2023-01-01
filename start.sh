91a607e8b4e17f89a4bc72b5c4a68ef02032aa43
AvirukBasak <avirukbasak@gmail.com>
2023-01-02 02:30:33 +0530

    added start script

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if ! ( which php > /dev/null ); then
+    echo 'error: php no installed'
+    exit 1
+fi
+
+php -S localhost:8080 --docroot=website-vuln/public/ 2>> vuln.log >> vuln.log &
+php -S localhost:42069 --docroot=website-evil/public/ 2>> evil.log >> evil.log &
+echo vuln server @ localhost:8080
+echo evil server @ localhost:42069
